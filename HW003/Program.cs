cbf5ceee2d3f2132e2823e3734d6c9ef21973365
altotass <altotass@gmail.com>
2022-08-27 08:56:14 +0300

    HW02_003

---
+++ HW003/Program.cs
@@ -0,0 +1,15 @@
+﻿// Задача 15: Напишите программу, которая принимает на вход цифру, 
+// обозначающую день недели, и проверяет, является ли этот день выходным.
+// 6 -> да
+// 7 -> да
+// 1 -> нет
+
+void Calendar()
+{
+    Console.WriteLine ("Напишите число от 1 до 7");
+    Console.WriteLine ("x= ");
+    int x = Convert.ToInt32(Console.ReadLine());
+    if (x > 5) Console.WriteLine("Да, выходной день");    
+    else Console.WriteLine("Нет, это будний день");
+}
+Calendar();
